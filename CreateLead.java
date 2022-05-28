e345a68e4d7a2497b4d822c1b93fe1ec77a01db2
NancyAndrew <105703194+NancyAndrew@users.noreply.github.com>
2022-05-28 17:06:22 +0530

    Add files via upload

---
+++ CreateLead.java
@@ -0,0 +1,48 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateLead {
+
+	
+	
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+ 
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Nancy");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Andrew");
+		driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Nans");
+		driver.findElement(By.name("departmentName")).sendKeys("Testing");
+		driver.findElement(By.id("createLeadForm_description")).sendKeys("Selenium Practice");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("florin_pops@yahoo.co.in");
+		WebElement ele = driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		Select province = new Select(ele);
+		province.selectByVisibleText("New York");
+		Thread.sleep(10000);
+		driver.findElement(By.name("submitButton")).click();
+		Thread.sleep(5000);
+		String title = driver.getTitle();
+		System.out.println("Titile of current page: "+title);
+		Thread.sleep(10000);
+		
+		driver.close();
+	}
+
+}
